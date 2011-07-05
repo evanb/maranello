dbef0f9d5bd7c9d3ed018ba238e696fb8b481727
Evan Bottcher <evan@bottch.com>
2011-07-05 20:37:30 +1000

    added wordpress

---
+++ wordpress/wp-content/themes/maranello/page_home.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Maranello Home Page
+*/
+?>                      
+
+Evan got here!
+
+<?php 
+
+// Use the default Genesis index.php 
+require_once(PARENT_DIR.'/index.php'); 
+
+?>
